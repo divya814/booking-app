8694e69ee8506403a522102ff6b9633292d06260
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-01-28 20:47:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import image1 from './assets/image1.jpg'
 import image2 from './assets/image2.jpg'
 import image3 from './assets/image3.jpg'
 import image4 from './assets/image4.jpg'
-import image5 from './assets/image5.jpg'
 import image6 from './assets/image6.jpg'
 import image7 from './assets/image7.jpg'
 import image8 from './assets/image8.jpg'
@@ -24,7 +23,7 @@ class App extends Component {
         </Fade>
 
         <BackgroundSlider
-          images={[image1, image2, image3, image4, image5, image6,image7,image8,image9,image10]}
+          images={[image1, image2, image3, image4, image6,image7,image8,image9,image10]}
           duration={8}
           transition={2}
         />
